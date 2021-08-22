44d019702cee4ccbe689f0c17f473b315f9980ab
Shaya Selincourt <sselincourt@gmail.com>
2021-08-21 21:59:55 -0700

    Fixed inconsistent styling due to client and server side sheets competing

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,7 @@
 import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/styles";
-// import createEmotionServer from '@emotion/server/create-instance';
 import theme from "../src/scripts/theme";
-// import createEmotionCache from '../src/createEmotionCache';
 
 export default class MyDocument extends Document {
   render() {
@@ -54,28 +52,12 @@ MyDocument.getInitialProps = async (ctx) => {
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
 
-  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
-  // However, be aware that it can have global side effects.
-  // const cache = createEmotionCache();
-  // const { extractCriticalToChunks } = createEmotionServer(cache);
-
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
-  // This is important. It prevents emotion to render invalid HTML.
-  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-  // const emotionStyles = extractCriticalToChunks(initialProps.html);
-  // const emotionStyleTags = emotionStyles.styles.map((style) => (
-  //   <style
-  //     data-emotion={`${style.key} ${style.ids.join(' ')}`}
-  //     key={style.key}
-  //     // eslint-disable-next-line react/no-danger
-  //     dangerouslySetInnerHTML={{ __html: style.css }}
-  //   />
-  // ));
 
   return {
     ...initialProps,
