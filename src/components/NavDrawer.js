2e4051d979cd9bf9f91d0cb019ad88e5711d04ed
Shaya Selincourt <sselincourt@gmail.com>
2021-08-25 21:02:03 -0700

    Fixed busted width on mobile; still need to implement dropdown menu here

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -1,9 +1,13 @@
+import React from "react";
 import {
   IconButton,
   SwipeableDrawer,
   List,
   ListItem,
   ListItemText,
+  Menu,
+  MenuItem,
+  KeyboardArrowDown,
 } from "@material-ui/core";
 import { ExpandMore, Close } from "@material-ui/icons";
 import { useState } from "react";
@@ -13,9 +17,10 @@ import theme from "../scripts/theme";
 
 const useStyles = makeStyles({
   list: {
-    padding: "10rem",
     height: `100vh`,
+    width: `100vw`,
     background: `${theme.palette.primary.main}`,
+    margin: `auto`,
   },
   linkText: {
     textDecoration: `none`,
@@ -37,7 +42,8 @@ const useStyles = makeStyles({
     color: `${theme.palette.orarion.main}`,
     fontFamily: "Book Antigua, serif",
     textAlign: `center`,
-    fontSize: `large`,
+    fontSize: `150%`,
+    padding: "1rem",
   },
   cancel: {
     display: "block",
@@ -49,7 +55,21 @@ const useStyles = makeStyles({
 const NavDrawer = ({ navLinks }) => {
   const classes = useStyles();
   const [state, setState] = useState({ top: false });
+  const [anchorEl1, setAnchorEl1] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
 
+  const handleClick1 = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const handleClick2 = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+  const handleClose1 = () => {
+    setAnchorEl1(null);
+  };
+  const handleClose2 = () => {
+    setAnchorEl2(null);
+  };
   const toggleDrawer = (anchor, open) => (event) => {
     if (
       event.type === "keydown" &&
@@ -59,7 +79,10 @@ const NavDrawer = ({ navLinks }) => {
     }
     setState({ [anchor]: open });
   };
-
+  const isOrarion = (link) => {
+    if (link === "ORARION") return classes.orarion;
+    else return classes.linkText;
+  };
   const navDrawerList = (anchor) => (
     <div
       className={classes.list}
@@ -68,17 +91,52 @@ const NavDrawer = ({ navLinks }) => {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List component="nav">
-        {navLinks.map(({ title, path }) => (
-          <Link href={path} key={title}>
-            <ListItem button>
-              <ListItemText
-                disableTypography
-                primary={title}
-                className={classes.linkText}
-              />
-            </ListItem>
-          </Link>
-        ))}
+        {navLinks.map(({ title, path }) => {
+          if (title === "Products") {
+            return (
+              <div key={title}>
+                <Menu
+                  id="simple-menu1"
+                  anchorEl={anchorEl1}
+                  keepMounted
+                  open={Boolean(anchorEl1)}
+                  onClose={handleClose1}
+                  onClick={handleClose1}
+                >
+                  <Link href={path[0].path}>
+                    <MenuItem>{path[0].title}</MenuItem>
+                  </Link>
+                  <Link href={path[1].path}>
+                    <MenuItem>{path[1].title}</MenuItem>
+                  </Link>
+                </Menu>
+                <ListItem
+                  button
+                  className={classes.itemHover}
+                  onClick={handleClick1}
+                >
+                  <ListItemText
+                    disableTypography
+                    primary={title}
+                    className={isOrarion(title)}
+                  />
+                </ListItem>
+              </div>
+            );
+          } else if (title === "Services") {
+          } else
+            return (
+              <Link href={path} key={title}>
+                <ListItem button className={classes.itemHover}>
+                  <ListItemText
+                    disableTypography
+                    primary={title}
+                    className={isOrarion(title)}
+                  />
+                </ListItem>
+              </Link>
+            );
+        })}
         <ListItem>
           <IconButton
             onClick={toggleDrawer("top", false)}
